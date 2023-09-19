e55b286251b73a3400c150c5a526718c577f3417
1939323749 <1939323749@qq.com>
2023-09-19 09:50:05 +0800

    fix: support ISO 8601 time format ics file

--- go.mod
+++ go.mod
@@ -14,6 +14,7 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/spf13/afero v1.9.5 // indirect
 	github.com/spf13/cast v1.5.1 // indirect
@@ -22,6 +23,7 @@ require (
 	github.com/subosito/gotenv v1.4.2 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
 import (
-	"github.com/apognu/gocal"
-	"github.com/spf13/viper"
-	"github.com/wneessen/go-mail"
+	"fmt"
 	"html/template"
 	"log"
 	"os"
 	"time"
+
+	"github.com/apognu/gocal"
+	"github.com/spf13/viper"
+	"github.com/wneessen/go-mail"
 )
 
 type Conf struct {
-	EmailFrom  string
-	EmailTo    string
-	STMPServer string
-	STMPPort   int
-	Username   string
-	Password   string
+	EmailFrom   string
+	EmailTo     string
+	STMPServer  string
+	STMPPort    int
+	Username    string
+	Password    string
+	IcsFileName string
 }
 
 func main() {
@@ -27,6 +30,9 @@ func main() {
 		log.Fatalf("failed to read config file: %s", err)
 	}
 	err := viper.Unmarshal(&conf)
+	if err != nil {
+		log.Fatal(err)
+	}
 	if err := m.From(conf.EmailFrom); err != nil {
 		log.Fatalf("failed to set From address: %s", err)
 	}
@@ -39,7 +45,7 @@ func main() {
 	err = m.SetBodyHTMLTemplate(tpl, struct {
 		Classes []class
 	}{
-		Classes: sortClasses(getTodayClasses()),
+		Classes: sortClasses(getTodayClasses(conf)),
 	})
 	if err != nil {
 		return
@@ -52,6 +58,7 @@ func main() {
 	if err := c.DialAndSend(m); err != nil {
 		log.Fatalf("failed to send mail: %s", err)
 	}
+	fmt.Fprintf(os.Stdout, "Send Success, %s", time.Now().String())
 }
 
 type class struct {
@@ -61,20 +68,32 @@ type class struct {
 	ClassRoom string
 }
 
-func getTodayClasses() []class {
+func getTodayClasses(conf Conf) []class {
 	var classes []class
 	// load ics file, you can get it from an app, download the app from https://www.wakeup.fun
-	f, _ := os.Open("myclasses.ics")
+	f, _ := os.Open(conf.IcsFileName)
 	defer f.Close()
 
 	// from now to end of the day
 	loc, _ := time.LoadLocation("Asia/Shanghai")
-	start := time.Now().In(loc)
+	start := time.Now().Local().UTC().In(loc)
 	end := time.Date(start.Year(), start.Month(), start.Day(), 23, 59, 59, 0, start.Location())
 
 	c := gocal.NewParser(f)
 	c.Start, c.End = &start, &end
 	c.Parse()
+
+	_, offsetSeconds := time.Now().Zone()
+	offset := time.Duration(offsetSeconds * int(time.Second))
+	if len(c.Events) > 0 {
+		timeValue := c.Events[0].RawStart.Value
+		if timeValue[len(timeValue)-1:] == "Z" {
+			for _, e := range c.Events {
+				classes = append(classes, class{Name: e.Summary, Start: e.Start.Add(offset).Format("15:04"), End: e.End.Add(offset).Format("15:04"), ClassRoom: e.Location})
+			}
+			return classes
+		}
+	}
 
 	for _, e := range c.Events {
 		classes = append(classes, class{Name: e.Summary, Start: e.Start.Format("15:04"), End: e.End.Format("15:04"), ClassRoom: e.Location})
